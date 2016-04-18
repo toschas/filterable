50dddd7f3eddd8c4718b7145a8ae7aa5980f4b57
Darjan Vukusic <darjan.vukusic@gmail.com>
2016-04-18 22:02:22 +0200

    filterable module

--- lib/filterable.rb
+++ lib/filterable.rb
@@ -1,5 +1,26 @@
 require "filterable/version"
 
 module Filterable
-  # Your code goes here...
+  module ClassMethods
+    def filter(filtering_params)
+      results = where(nil)
+      filtering_params.each do |key, value|
+        results = results.public_send(key, value) if results.respond_to? key
+      end
+      results
+    end
+
+    def filter_by(*args)
+      args.each do |filt|
+        self.define_singleton_method(
+          "by_#{filt}", 
+          ->(value) { self.send(:where, { filt => value }) }
+        )
+      end
+    end
+  end
+
+  def self.included(base)
+    base.extend ClassMethods
+  end
 end
