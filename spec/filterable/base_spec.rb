db98160fcd03a5dd256a6f0916e7a618d10fee9c
Darjan Vukusic <darjan.vukusic@gmail.com>
2016-05-06 15:22:58 +0200

    use dashboard model in base and generator specs

--- spec/filterable/base_spec.rb
+++ spec/filterable/base_spec.rb
@@ -1,24 +1,20 @@
 require 'spec_helper'
-require 'dummy_app/app'
 
 module Filterable
   describe Base do
-    before :all do 
-      class SimpleModel < ActiveRecord::Base; end
-      App.init
-    end
-
     it 'provides .filter_by to the model' do
-      expect(SimpleModel).to respond_to :filter_by
+      expect(Dashboard).to respond_to :filter_by
     end
 
     it 'calls #generate on Generator when .filter_by is invoked' do
       filters = [:name, :title]
-      generator = double(Generator.new(SimpleModel, filters))
-      expect(Generator).to receive(:new).with(SimpleModel, filters).and_return(generator)
+      generator = double(Generator.new(Dashboard, filters))
+      expect(Generator).to(
+        receive(:new).with(Dashboard, filters).and_return(generator)
+      )
       expect(generator).to receive(:generate)
 
-      SimpleModel.filter_by *filters
+      Dashboard.filter_by *filters
     end
   end
 end
--- spec/filterable/generator_spec.rb
+++ spec/filterable/generator_spec.rb
@@ -1,44 +1,39 @@
 require 'spec_helper'
-require 'dummy_app/schema_helper'
 
 module Filterable
   describe Generator do
     describe '.generate' do
-      before :all do 
-        class SimpleModel < ActiveRecord::Base; end
-      end
-
       it 'generates filter methods' do
-        Generator.new(SimpleModel, [:name, :title]).generate
+        Generator.new(Dashboard, [:name, :title]).generate
 
-        expect(SimpleModel).to respond_to :filter
-        expect(SimpleModel).to respond_to :by_name
-        expect(SimpleModel).to respond_to :by_title
+        expect(Dashboard).to respond_to :filter
+        expect(Dashboard).to respond_to :by_name
+        expect(Dashboard).to respond_to :by_title
       end
 
       it 'generates filter scopes for joined models' do
         user_filters = [:user_id, joins: :user]
         another_model_filters = [:another_model_another_attribute, 
                                  joins: :another_model]
-        Generator.new(SimpleModel, user_filters).generate
-        Generator.new(SimpleModel, another_model_filters).generate
+        Generator.new(Dashboard, user_filters).generate
+        Generator.new(Dashboard, another_model_filters).generate
 
-        expect(SimpleModel).to respond_to :by_user_id
-        expect(SimpleModel).to respond_to :by_another_model_another_attribute
+        expect(Dashboard).to respond_to :by_user_id
+        expect(Dashboard).to respond_to :by_another_model_another_attribute
       end
 
       it 'generates filter scopes for nested joined models' do
         filters = [:user_post_title, joins: { user: :post }]
-        Generator.new(SimpleModel, filters).generate
+        Generator.new(Dashboard, filters).generate
 
-        expect(SimpleModel).to respond_to :by_user_post_title
+        expect(Dashboard).to respond_to :by_user_post_title
       end
 
       it 'generates custom filter method if custom option is sent' do
         filters = [:custom_filter, custom: true]
-        Generator.new(SimpleModel, filters).generate
+        Generator.new(Dashboard, filters).generate
 
-        expect(SimpleModel).to respond_to :by_custom_filter
+        expect(Dashboard).to respond_to :by_custom_filter
       end
     end
 
