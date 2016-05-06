770536123033cb057925e1fd21288300e0dfb704
Darjan Vukusic <darjan.vukusic@gmail.com>
2016-05-06 14:10:31 +0200

    fix schema helper, class methods

--- spec/support/schema_helper.rb
+++ spec/support/schema_helper.rb
@@ -1,22 +1,27 @@
 class SchemaHelper
-  def connect_to(adapter, database)
-     ActiveRecord::Base.establish_connection(
-       adapter: 'sqlite3',
-       database: ':memory:'
-     )
-     self
-  end
+  class << self
+    def connect_to(adapter, database)
+       ActiveRecord::Base.establish_connection(
+         adapter: adapter,
+         database: database
+       )
+       self
+    end
 
-  def generate_model(model_name, attributes)
-     ActiveRecord::Schema.define do
-       create_table model_name.pluralize, force: true do |t|
-         attributes.each do |attribute, column_type|
-           t.send(column_type, attribute)
+    def generate_model(model_name, attributes)
+       ActiveRecord::Schema.define do
+         create_table model_name.to_s.pluralize, force: true do |t|
+           attributes.each do |attribute, column_type|
+             t.send(column_type, attribute)
+           end
          end
        end
-     end
 
-     Object.const_set(model_name.classify, Class.new(ActiveRecord::Base))
-     self
+       unless Object.const_defined? model_name.to_s.classify
+         Object.const_set(model_name.to_s.classify, Class.new(ActiveRecord::Base)) 
+       end
+
+       self
+    end
   end
 end
