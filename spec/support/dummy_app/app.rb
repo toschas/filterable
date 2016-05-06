e1a25238bcab4ae1b9256cb4c2b0a52b43776f33
Darjan Vukusic <darjan.vukusic@gmail.com>
2016-05-06 14:15:31 +0200

    add dummy app with models for easier testing

---
+++ spec/support/dummy_app/app.rb
@@ -0,0 +1,38 @@
+require 'active_record'
+require_relative 'models'
+require 'support/schema_helper'
+
+class App
+  class << self
+    def init
+      database_connect
+      generate_schema
+      create_models
+    end
+
+    def database_connect
+      SchemaHelper.connect_to('sqlite3', ':memory:')
+    end
+
+    def generate_schema
+      SchemaHelper
+        .generate_model(:user, { name: 'string' }) 
+        .generate_model(:role, { name: 'string' }) 
+        .generate_model(:company, { name: 'string' }) 
+        .generate_model(:project, { name: 'string' }) 
+        .generate_model(:task, { name: 'string' }) 
+    end
+
+    def create_models
+      3.times do |n|
+        User.create(name: "user#{n}")
+        Role.create(name: "role#{n}")
+        Company.create(name: "company#{n}")
+        Project.create(name: "project#{n}")
+        Task.create(name: "task#{n}")
+      end
+    end
+  end
+end
+
+
---
+++ spec/support/dummy_app/models.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+end
+
+class Role <ActiveRecord::Base
+end
+
+class Company < ActiveRecord::Base
+end
+
+class Project < ActiveRecord::Base
+end
+
+class Task < ActiveRecord::Base
+end
+
